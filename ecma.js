a4a2a9f75b0c8332aa3aa7d19927855bb3d44d52
Saurabh Mishra <saurabhmishra4894@gmail.com>
2019-04-18 12:27:26 +0530

    added commit

--- ecma.js
+++ ecma.js
@@ -1,6 +1,7 @@
 if(true){
 
 document.write("CHanged something for loop");
+document.write("CHanged something for second loop");
 document.write("CHanged new for loop");
 document.write("CHanged something for third loop");
 
