c9561609073555e2db0910dcb20885dae42eec38
Saurabh Mishra <saurabhmishra4894@gmail.com>
2019-04-18 11:47:14 +0530

    some commit
    # Conflicts:
    #	ecma.js

--- ecma.js
+++ ecma.js
@@ -1,7 +1,6 @@
 if(true){
 
 document.write("CHanged something for loop");
-document.write("CHanged something for second loop");
 document.write("CHanged new for loop");
 document.write("CHanged something for third loop");
 
